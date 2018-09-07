d6e4fdd4d8562f4a88f60f30d2b80df2aa2523ce
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-09-07 12:23:24 -0400

    Add hover css to buttons

---
+++ public/scripts/focus-button.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.button-container').hover(
+        function() {
+            $(this).css({opacity: '1'});
+            $('.button-body').css({'text-decoration': 'underline'})
+        },
+        function() {
+            $(this).css({opacity:'0.7'});
+            $('.button-body').css({'text-decoration': 'none'})
+        }
+    );
+})
