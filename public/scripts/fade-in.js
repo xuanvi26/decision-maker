e09426c8dd5e10a116905b26ff6f6ecb3cff981a
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-09-07 15:59:02 -0400

    Add fade-in to welcome page

---
+++ public/scripts/fade-in.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#title-section.hidden').fadeIn(1000).removeClass('hidden');
+    setTimeout(function() {
+        $('#description-section.hidden').fadeIn(1000).removeClass('hidden');
+    }, 1000);
+});
