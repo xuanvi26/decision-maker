a45fb17399ad87124c0c9bd585e16de2241b42bb
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-09-11 12:01:46 -0400

    Create complete poll page and manage routes

--- server.js
+++ server.js
@@ -55,6 +55,14 @@ app.get('/createpoll/step/2', (req, res) => {
   res.render('create-poll-step-2');
 });
 
+app.post('/createpoll/complete', (req, res) => {
+  res.redirect('/createpoll/complete');
+})
+
+app.get('/createpoll/complete', (req, res) => {
+  res.render('create-poll-complete');
+})
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
